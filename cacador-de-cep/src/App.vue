b6c516a6bdce918e1c665833c5384a074ed24958
Guilherme Tamanini <guilherme.gui.tama@gmail.com>
2022-09-21 19:14:24 -0300

    Css att
    Css +

--- cacador-de-cep/src/App.vue
+++ cacador-de-cep/src/App.vue
@@ -29,35 +29,67 @@ export default {
           v-model="cep"
         />
       </div>
+
       <div class="mb-3">
-        <button type="submit" id="ho" class="form-control btn btn-primary">Procurar</button>
+        <button type="submit" id="ho" class="form-control btn btn-primary">
+          Procurar
+        </button>
       </div>
     </form>
     <form class="end">
-      <label for="rua">Rua</label>
-      <input class="form-control" type="text" v-model="endereco.logradouro" />
-      <label for="=num">bairro</label>
-      <input class="form-control" type="text" v-model="endereco.bairro" />
-      <label for="cidade">cidade</label>
-      <input class="form-control" type="text" v-model="endereco.localidade" />
-      <label for="uf">UF</label>
-      <input class="form-control" type="text" v-model="endereco.uf" />
-      <label for="=ibge">ibge</label>
-      <input class="form-control" type="text" v-model="endereco.ibge" />
-      <label for="ddd">DDD</label>
-      <input class="form-control" type="text" v-model="endereco.ddd" />
-      <label for="siafi">siafi</label>
-      <input class="form-control" type="text" v-model="endereco.siafi" />
+      <div class="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Rua</span>
+        <input class="form-control" type="text" v-model="endereco.logradouro" />
+      </div>
+
+      <div class="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Bairro</span>
+        <input class="form-control" type="text" v-model="endereco.bairro" />
+      </div>
+
+      <div class="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Cidade</span>
+        <input class="form-control" type="text" v-model="endereco.localidade" />
+      </div>
+
+      <div class="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">UF</span>
+        <input class="form-control" type="text" v-model="endereco.uf" />
+      </div>
+
+      <div class="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Ibge</span>
+        <input class="form-control" type="text" v-model="endereco.ibge" />
+      </div>
+
+      <div class="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">DDD</span>
+        <input class="form-control" type="text" v-model="endereco.ddd" />
+      </div>
+
+      <div class="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Siafi</span>
+        <input class="form-control" type="text" v-model="endereco.siafi" />
+      </div>
+
     </form>
   </div>
 </template>
 <style>
+span {
+  max-width: 100px;
+  width: 60px;
+  justify-content: center;
+  background-color: #0d6efd;
+}
 button {
   max-width: 100px;
   padding-left: 200px;
 }
 div {
   flex-direction: row;
+  margin-top: 10px;
+  margin-bottom: 10px;
 }
 h2 {
   text-align: center;
